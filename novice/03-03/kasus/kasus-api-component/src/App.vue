06583263957fb6f40c5b706fb0e0b8f67dea0527
ranggatw <rangga.public@gmail.com>
2021-04-28 14:16:14 +0700

    week 3 day 3

---
+++ novice/03-03/kasus/kasus-api-component/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Kasus />
+  </div>
+</template>
+
+<script>
+import Kasus from "@/components/Kasus";
+
+export default {
+  name: "App",
+  components: {
+    Kasus,
+  },
+};
+</script>
+
+<style>
+/* #app { */
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+/* } */
+</style>
---
+++ novice/03-03/kasus/kasus-api-component/src/components/Kasus.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h4>Detail Post</h4>
+    <div v-for="post in posts.slice(0, 5)" :key="post.userId + Math.random()">
+      <p>Ini id dari Post : {{ post.id }}</p>
+      <p>Ini title Post : {{ post.title }}</p>
+      <p>Ini isi dari Post : {{ post.body }}</p>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    posts: "",
+  }),
+  methods: {
+    async getPost() {
+      try {
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        this.posts = res.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  mounted() {
+    this.getPost();
+  },
+};
+</script>
